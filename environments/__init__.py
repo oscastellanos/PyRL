a92db52eb2fdd031de49316e7b7f6aea8d3e4654
Osvaldo Castellanos <ocstllns@gmail.com>
2019-02-07 13:37:49 -0600

    Initial commit

---
+++ environments/__init__.py
@@ -0,0 +1,13 @@
+try:
+    from environments.doom import Doom
+except:
+    print("Couldn't import doom")
+#from environments.catcher import Catcher
+#from ple.games.flappybird import FlappyBird
+#from ple.games.monsterkong import MonsterKong
+#from ple.games.pixelcopter import Pixelcopter
+#from ple.games.pong import Pong
+#from ple.games.puckworld import PuckWorld
+#from ple.games.raycastmaze import RaycastMaze
+from environments.snake import Snake
+#from ple.games.waterworld import WaterWorld
